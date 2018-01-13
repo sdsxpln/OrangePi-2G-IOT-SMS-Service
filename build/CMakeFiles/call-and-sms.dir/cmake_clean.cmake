c1fd0d421c2e61d9a265b7c128508dc53e2c5c5f
AleksandrSae <saechnikov.a@yandex.ru>
2018-01-13 20:46:28 +0000

    Add some CMakeFiles

---
+++ build/CMakeFiles/call-and-sms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/call-and-sms.dir/src/src-test-call-and-sms.c.o"
+  "call-and-sms.pdb"
+  "call-and-sms"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/call-and-sms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
